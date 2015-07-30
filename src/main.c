6268931ad5e61103652f134594903a238c62e5a6
Nils Ceberg <nils.ceberg@gmail.com>
2015-07-30 03:49:39 +0200

    C -> C++

--- src/main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Cuilien agar.io bot brain\n");
-	return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Cuilien agar.io brain" << std::endl;
+
+	return 0;
+}
